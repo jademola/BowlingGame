1d3888e4a59bd39b03f5405f0b68b91161e67381
Jimi Ademola <jimi.ademola1@gmail.com>
2025-02-12 18:33:13 -0800

    Added LaunchBall components

---
+++ Assets/BallController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallController : MonoBehaviour
+{
+    
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
+
+    void Start()
+    {
+        //Get a reference to Rigidbody
+        ballRB = GetComponent<Rigidbody>();
+
+        // Launch ball when the space bar is pressed
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall() 
+    {
+        ballRB.addForce(transform.forward * force, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+}
